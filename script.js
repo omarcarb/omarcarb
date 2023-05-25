07199edcaa0f1e8e517bbf91de271b71e74e0d75
Omar Carbajal <33193266+omarcarb@users.noreply.github.com>
2023-05-25 11:56:12 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-toggle');
+
+navToggle.onclick = function() {
+    const visibility = primaryNav.getAttribute('data-visible');
+    
+    if(visibility === "false"){
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute('aria-expanded', true);
+    }
+    else if(visibility === "true"){
+        primaryNav.setAttribute("data-visible", false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+};
