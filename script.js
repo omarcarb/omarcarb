ec5368a83485d857d3b5c78586124b7347431184
Omar Carbajal <33193266+omarcarb@users.noreply.github.com>
2023-06-01 11:37:43 -0700

    New Changes

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
 const primaryNav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-toggle');
+const homeLink = document.querySelector('.home-link');
+var path = window.location.pathname;
+var page = path.split("/").pop();
+console.log( page );
+
+
+const landingImage = document.querySelector(".landing-image");
+
+window.onload = function(){
+    if(page === "weatherlerts.html"){
+        landingImage.src = "assests/png/weatherlerts-mockup.png"
+    }
+    else if(page === "shelter_finds.html"){
+        landingImage.src = "assests/png/shelter-finds-mockup.png"
+    }
+    else if(page === "free_roam.html"){
+        landingImage.src = "assests/png/free-roam-mockup.png"
+    }
+}
+
+const sectionOneOptions = {
+    rootMargin: '-100px 0px 20px 0px'
+};
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".landing-image")
+
+const sectionOneObserver = new IntersectionObserver(function(
+    entries, 
+    sectionOneObserver
+    ) {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                header.classList.add("header-scrolled");
+            }
+            else{
+                header.classList.remove("header-scrolled");
+            }
+        })
+    }, sectionOneOptions)
+
+sectionOneObserver.observe(sectionOne);
+
 
 navToggle.onclick = function() {
     const visibility = primaryNav.getAttribute('data-visible');
@@ -13,3 +55,4 @@ navToggle.onclick = function() {
         navToggle.setAttribute('aria-expanded', false);
     }
 };
+
