50525e6e031897310494cf94bfbba7173def0884
Omar Carbajal <33193266+omarcarb@users.noreply.github.com>
2023-06-07 21:30:07 -0700

    Subtle Animations

--- script.js
+++ script.js
@@ -39,6 +39,31 @@ const header = document.querySelector("header");
 const sectionOne = document.querySelector(".landing-image")
 const navIcon = document.querySelector(".mobile-toggle");
 
+const faders = document.querySelectorAll(".section");
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: '0px 0px -100px 0px'
+};
+const appearOnScroll = new IntersectionObserver(function(
+    entries, 
+    appearOnScroll
+    ){
+        entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                return;
+            }
+            else{
+                entry.target.classList.add('animation');
+                appearOnScroll.unobserve(entry.target);
+            }
+        })
+    }, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader)
+});
+
 const sectionOneObserver = new IntersectionObserver(function(
     entries, 
     sectionOneObserver
@@ -54,7 +79,6 @@ const sectionOneObserver = new IntersectionObserver(function(
             }
         })
     }, sectionOneOptions)
-
 sectionOneObserver.observe(sectionOne);
 
 
