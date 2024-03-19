e28f1962eed452fe1f0dc4161c5816ae464b9ef3
Omar Carbajal <omarcarb04@gmail.com>
2024-03-18 17:00:37 -0700

    added parallax effect

--- script.js
+++ script.js
@@ -1,81 +1,17 @@
-// nav bar elements for mobile devices //
-const menuButton = document.querySelector(".mobile-toggle")
-const navigationElements = document.querySelector(".primary-navigation")
-
-menuButton.onclick = function() {
-    const visibility = navigationElements.getAttribute('data-visible');
-    
-    if(visibility === "false"){
-        navigationElements.setAttribute("data-visible", true);
-        menuButton.setAttribute('aria-expanded', true);
-        disableScroll();
-    }
-    else if(visibility === "true"){
-        navigationElements.setAttribute("data-visible", false);
-        menuButton.setAttribute('aria-expanded', false);
-        enableScroll();
-    }
-};
-
-// for closing menu when clicking on contact button on mobile  //
-
-const contactButton = document.querySelector(".button__contact")
-
-contactButton.onclick = function(){
-    const visibility = navigationElements.getAttribute('data-visible');
-
-    if(visibility == "true"){
-        navigationElements.setAttribute("data-visible", false);
-        menuButton.setAttribute('aria-expanded', false);
-    }
-    else{
-        return;
-    }
-    console.log(visibility);
-}
 
 // for parallax scroll effect
-let background = document.getElementById("background");
-let foreground = document.getElementById("foreground");
-let foreground1 = document.getElementById("foreground1");
-let foreground2 = document.getElementById("foreground2");
+let foreground = document.getElementsByClassName("wave")[0];
+let foreground1 = document.getElementsByClassName("wave")[1];
+let foreground2 = document.getElementsByClassName("wave")[2];
 
 
 window.addEventListener("scroll", function(){
     let value = window.scrollY;
 
-    foreground.style.top = value * .3 + 'px';
-    foreground1.style.top = value * .2 + 'px';
-    foreground2.style.top = value * .1 + 'px';
-    background.style.top = value * .08 + 'px';
+    foreground.style.bottom = value * .4 + 'px';
+    foreground1.style.bottom = value * .3 + 'px';
+    foreground2.style.bottom = value * 0 + 'px';
 })
 
-console.log(window.innerWidth);
 
-function disableScroll(){
-    scrollTop = window.scrollY || document.documentElement.scrollTop;
-    scrollLeft = window.scrollX || document.documentElement.scrollLeft;
 
-    window.onscroll = function (){
-        console.log(scrollLeft + scrollTop)
-        window.scrollTo(scrollLeft, scrollTop);
-    };
-}
-function enableScroll(){
-    window.onscroll = function(){
-    };
-}
-
-const tags = document.querySelectorAll("tags");
-
-var timer = 0;
-var maxTime = 40
-
-function timer (){
-    while (timer <= maxTime){
-        setTimeout(function(){
-            timer++
-        }, 1000)
-        console.log(timer)
-    }
-}
